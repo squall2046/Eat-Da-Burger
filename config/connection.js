60bf53d9d728ab6002fa498ebbc9b295acd2692c
squall2046 <squall2046@hotmail.com>
2019-02-26 04:53:45 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,18 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+        connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+};
 
 connection.connect((err) => {
     if (err) {
