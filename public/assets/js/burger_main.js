2c5708c16fd234e539d58f88f9940a247ae0d252
squall2046 <squall2046@hotmail.com>
2019-02-27 16:09:14 -0500

    update

--- public/assets/js/burger_main.js
+++ public/assets/js/burger_main.js
@@ -1,24 +1,38 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
+$(function () {
   //////////////// create new burger from input form ////////////////
   $(".newForm").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
+    let clientInput = $("#burgerInput").val().trim();
     let newBurger = {
-      burger_name: $("#burgerInput").val().trim(),
+      burger_name: clientInput,
       devoured: 0
       // in DB, devoured is boolean: 0 is false; 1 is true
     };
     console.log(newBurger)
 
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(function () {
-      location.reload();
-    });
+    if (clientInput.length < 1) {
+      Swal.fire({
+        // background: '#fff url("/assets/img/burger-load.gif")',
+        text: 'What burger would you like to have?',
+        // title: "Hi!!",
+        animation: false,
+        imageUrl: '/assets/img/burger-load.gif',
+        imageWidth: 100,
+        imageHeight: 100,
+        customClass: 'animated fadeIn'
+      })
+    } else {
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(function () {
+        location.reload();
+      });
+    }
   });
 
   //////////////// update burger condition from noDevour/devoured list to devoured/noDevour list ////////////////
